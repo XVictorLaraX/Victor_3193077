a3b99cfccf3e1b60bf0a3af4db3e324c63d5998a
XVictorLaraX <victorlara.informatica@gmail.com>
2024-09-08 10:45:18 -0600

    Actividad 02/09/2024

---
+++ Tarea_Pages/app.js
@@ -0,0 +1,5 @@
+// Funcion sumar de la tarea
+function suma(a= prompt("Dame el primer numero"), b= prompt("Dame el segundo numero")){
+    return parseInt(a)+ parseFloat(b);
+}
+console.log(suma());
