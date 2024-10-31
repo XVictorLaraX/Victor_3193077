30be578fcea574d6648b0c45b5edd1c480f0c45a
XVictorLaraX <victorlara.informatica@gmail.com>
2024-10-30 22:59:36 -0600

    Actividad: 23/10/2024

---
+++ Rutas_Express/routes/pages.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+// Crear un router de Express
+const router = express.Router();
+
+// Ruta para la Página 1
+router.get('/inicio', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+// Ruta para la Página 2
+router.get('/calculadora', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'calculadora.html'));
+});
+
+// Ruta para la Página 3
+router.get('/qr', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'qr.html'));
+});
+
+// Exportar el router para usarlo en la aplicación principal
+module.exports = router;
