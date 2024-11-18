688ed4dc7ae342f00491adf22b3259dc52a08e08
XVictorLaraX <victorlara.informatica@gmail.com>
2024-11-17 19:34:40 -0600

    Actividad 21/08/2024

---
+++ Maquina_Adivinar/app.js
@@ -0,0 +1,23 @@
+//Operaciones
+let suma = 1+1;
+console.log(suma);
+
+let resta = 5-4;
+console.log(resta);
+
+let mult=2*2;
+console.log(mult);
+
+//-----------------
+// Operadores Relacionales
+
+
+let resultado = 20 >= 10;
+console.log(resultado);
+ 
+let dist = 10 != 10;
+console.log("Dist: "+dist);
+
+let compara = 20 === 56;
+console.log(compara);
+
